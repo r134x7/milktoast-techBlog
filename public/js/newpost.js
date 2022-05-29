535e32482fe71ca5979a17677a11138bc8c547db
r134x7 <r134x7@gmx.com>
2022-05-30 03:55:10 +1000

    got create new post to work

---
+++ public/js/newpost.js
@@ -0,0 +1,49 @@
+// taken from 14-MVC/28-Stu_Mini-Project/public/js/profile.js
+
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('#post-content').value.trim();
+
+  if (title && content) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+document
+  .querySelector('.post-form')
+  .addEventListener('submit', newFormHandler);
+
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
+
+//     const response = await fetch(`/api/projects/${id}`, {
+//       method: 'DELETE',
+//     });
+
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to delete project');
+//     }
+//   }
+// };
+
+
+// document
+//   .querySelector('.project-list')
+//   .addEventListener('click', delButtonHandler);
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,16 +3,5 @@ module.exports = {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
     },
-
-    click_button: () => {
-        // when signup is clicked the page changes render
-        return document
-            .querySelector('#signup').addEventListener("click", function () {
-                console.log("yikes");
-            })
-            ;
-        // when login is clicked the page changes render
-
-    }
   };
   
