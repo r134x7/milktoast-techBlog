602ac73d2f0a42f8497222a935877a82fe637683
r134x7 <r134x7@gmx.com>
2022-05-30 02:53:13 +1000

    fixed issue with getting login and signup to work

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+// taken from 14-MVC/28-Stu_Mini-Project/public/js/login.js
+
+// this had to be separated from the login.js script otherwise an error occurs due to missing forms it expects
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,5 +3,16 @@ module.exports = {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
     },
+
+    click_button: () => {
+        // when signup is clicked the page changes render
+        return document
+            .querySelector('#signup').addEventListener("click", function () {
+                console.log("yikes");
+            })
+            ;
+        // when login is clicked the page changes render
+
+    }
   };
   
